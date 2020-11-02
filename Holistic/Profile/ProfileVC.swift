dc67173f73de65d8677e236be93411884073e679
akbarikeyur <keyurdakbari@gmail.com>
2020-11-02 22:33:18 +0530

    Profile

---
+++ Holistic/Profile/ProfileVC.swift
@@ -0,0 +1,38 @@
+//
+//  ProfileVC.swift
+//  Holistic
+//
+//  Created by Keyur on 02/11/20.
+//  Copyright © 2020 Keyur Akbari. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK:- Button click event
+    @IBAction func clickToSideMenu(_ sender: Any) {
+        self.menuContainerViewController.toggleLeftSideMenuCompletion {}
+    }
+    
+    @IBAction func clickToNotification(_ sender: Any) {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
